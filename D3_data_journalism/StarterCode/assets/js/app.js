537142dc3f06809fd39db231fcc1ccc1cb0678e9
LexiJess <32692730+LexiJess@users.noreply.github.com>
2021-01-22 19:31:30 -0700

    updating missed corrections in js

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -14,7 +14,7 @@ var height = svgHeight - margin.top - margin.bottom;
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 var svg = d3
-  .select(".chart")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -94,22 +94,24 @@ function updateToolTip(chosenXAxis, circlesGroup) {
 }
 
 // Retrieve data from the CSV file and execute everything below
-d3.csv("data.csv").then(function(incomeData, err) {
+d3.csv("./assets/data/data.csv").then(function(incomeData, err) {
   if (err) throw err;
 
   // parse data
   incomeData.forEach(function(data) {
-    data.income = +data.income;
-    data.num_hits = +data.num_hits;
-    data.num_albums = +data.num_albums;
+    data.income =+ data.income;
+    data.age =+ data.age;
+    data.obesity =+ data.obesity;
   });
+
+  console.log(incomeData);
 
   // xLinearScale function above csv import
   var xLinearScale = xScale(incomeData, chosenXAxis);
 
   // Create y scale function
   var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(incomeData, d => d.num_hits)])
+    .domain([0, d3.max(incomeData, d => d.age)])
     .range([height, 0]);
 
   // Create initial axis functions
@@ -132,7 +134,7 @@ d3.csv("data.csv").then(function(incomeData, err) {
     .enter()
     .append("circle")
     .attr("cx", d => xLinearScale(d[chosenXAxis]))
-    .attr("cy", d => yLinearScale(d.num_hits))
+    .attr("cy", d => yLinearScale(d.age))
     .attr("r", 20)
     .attr("fill", "pink")
     .attr("opacity", ".5");
@@ -141,17 +143,17 @@ d3.csv("data.csv").then(function(incomeData, err) {
   var labelsGroup = chartGroup.append("g")
     .attr("transform", `translate(${width / 2}, ${height + 20})`);
 
-  var hairLengthLabel = labelsGroup.append("text")
+  var incomeLabel = labelsGroup.append("text")
     .attr("x", 0)
     .attr("y", 20)
     .attr("value", "income") // value to grab for event listener
     .classed("active", true)
-    .text("Hair Metal Ban Hair Length (inches)");
+    .text("Income");
 
-  var albumsLabel = labelsGroup.append("text")
+  var ageLabel = labelsGroup.append("text")
     .attr("x", 0)
     .attr("y", 40)
-    .attr("value", "num_albums") // value to grab for event listener
+    .attr("value", "age") // value to grab for event listener
     .classed("inactive", true)
     .text("Age");
 
@@ -162,7 +164,7 @@ d3.csv("data.csv").then(function(incomeData, err) {
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
     .classed("axis-text", true)
-    .text("Number of Billboard 500 Hits");
+    .text("Income");
 
   // updateToolTip function above csv import
   var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
@@ -205,7 +207,7 @@ d3.csv("data.csv").then(function(incomeData, err) {
           ageLabel
             .classed("active", false)
             .classed("inactive", true);
-          hairLengthLabel
+          incomeLabel
             .classed("active", true)
             .classed("inactive", false);
         }
